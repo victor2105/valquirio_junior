9f7ab65250bd22b669a85056cb17b8f81bf888de
vinic-i <vinicius.snts08@gmail.com>
2020-11-28 00:57:17 -0300

    footer 3

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,12 +1,11 @@
 <script>
-	import { Hero, Reviews, Smallhero } from '../components'
+	import { Hero, Reviews, Smallhero } from "../components";
 </script>
+
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<Hero></Hero>
-
-<Reviews></Reviews>
-
-<Smallhero></Smallhero>
+<Hero />
+<Smallhero />
+<Reviews />
