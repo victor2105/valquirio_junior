be2fa7a32d8fb06f6d24ab3c2959b5f84b5fbd8c
victor2105 <victor_henri_que@hotmail.com>
2020-11-27 23:18:35 -0500

    update smallhero

--- site/src/components/home/Smallhero.svelte
+++ site/src/components/home/Smallhero.svelte
@@ -4,7 +4,7 @@
             <div class="col-lg-8 mx-auto">
                 <h2 class="text-white mb-4">Formas de Atendimento</h2>
                 <p class="text-white-50">
-                    Além do atendimento realizado no <a target="_blank" href="https://www.instagram.com/ikigaidojosc/?hl=pt-br"> @ikigaidojosc </a>, também atendemos em domicílio com todo conforto profissional.
+                    Além do atendimento realizado no <a target="_blank" href="https://www.instagram.com/p/CGcixOSAOEl/?utm_source=ig_web_copy_link"> @ikigaidojosc </a>, também atendemos em domicílio com todo conforto profissional.
                 </p>
             </div>
         </div>
