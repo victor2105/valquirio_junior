85d478908743fd3bbc4d2b09410df8b33da591f1
vinic-i <vinicius.snts08@gmail.com>
2020-11-28 03:03:34 -0300

    logo nav

--- site/src/components/Nav.svelte
+++ site/src/components/Nav.svelte
@@ -1,26 +1,52 @@
 <script>
 	let y = 0;
 	let open = false;
-
 </script>
 
-<svelte:window bind:scrollY={y}/>
+<svelte:window bind:scrollY={y} />
 
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top {y > 100 ? 'navbar-shrink' : '' } " id="mainNav">
+<nav
+	class="navbar navbar-expand-lg navbar-light fixed-top {y > 100 ? 'navbar-shrink' : ''} "
+	id="mainNav">
 	<div class="container">
+		<img src="assets/logos/logo2.png" class="pr-2" width="50" alt="logo" />
 
-		<a class="navbar-brand logo-font  {y > 100 ? 'text-gradient' : '' } " on:click={() => open = false} href="#page-top">Kirro Mass</a>
+		<a
+			class="navbar-brand logo-font  {y > 100 ? 'text-gradient' : ''} "
+			on:click={() => (open = false)}
+			href="#page-top">Kiro Mass</a>
 
-		<button class="navbar-toggler navbar-toggler-right" type="button" on:click={() => open = !open} aria-expanded={open}>
+		<button
+			class="navbar-toggler navbar-toggler-right"
+			type="button"
+			on:click={() => (open = !open)}
+			aria-expanded={open}>
 			Menu
-			<i class="fas fa-bars"></i>
+			<i class="fas fa-bars" />
 		</button>
-		<div class="collapse navbar-collapse {open ? 'show' : ''}" id="navbarResponsive">
+		<div
+			class="collapse navbar-collapse {open ? 'show' : ''}"
+			id="navbarResponsive">
 			<ul class="navbar-nav ml-auto">
-				<li class="nav-item"><a class="nav-link js-scroll-trigger" on:click={() => open = false} href="#about">Sobre</a></li>
-				<li class="nav-item"><a class="nav-link js-scroll-trigger" on:click={() => open = false} href="#projects">Comentários</a></li>
-				<li class="nav-item"><a class="nav-link js-scroll-trigger" on:click={() => open = false} href="#signup">Contatos</a></li>
+				<li class="nav-item">
+					<a
+						class="nav-link js-scroll-trigger"
+						on:click={() => (open = false)}
+						href="#about">Sobre</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link js-scroll-trigger"
+						on:click={() => (open = false)}
+						href="#projects">Comentários</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link js-scroll-trigger"
+						on:click={() => (open = false)}
+						href="#signup">Contatos</a>
+				</li>
 			</ul>
 		</div>
 	</div>
