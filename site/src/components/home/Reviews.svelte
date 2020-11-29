6a959c1a348b18e127f41ab427bc58175ee9f5ad
victor2105 <victor_henri_que@hotmail.com>
2020-11-28 23:51:43 -0500

    informações centralizadas

--- site/src/components/home/Reviews.svelte
+++ site/src/components/home/Reviews.svelte
@@ -1,25 +1,51 @@
+<script>
+    import { wpLink } from '../../config'
+</script>
 <section class="projects-section bg-light" id="projects">
     <div class="container">
         <!-- Featured Project Row-->
         <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-            <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/IMG_20201009_174550_700.jpg" alt="" /></div>
+            <div class="col-xl-8 col-lg-7">
+                <img
+                    class="img-fluid mb-3 mb-lg-0"
+                    src="assets/IMG_20201009_174550_700.jpg"
+                    alt="" />
+            </div>
             <div class="col-xl-4 col-lg-5">
-                <div class="text-gradient featured-text text-center text-lg-left">
+                <div
+                    class="text-gradient featured-text text-center text-lg-left">
                     <h4>Shoreline</h4>
-                    <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                    <p class="text-black-50 mb-0">
+                        Grayscale is open source and MIT licensed. This means
+                        you can use it for any project - even commercial
+                        projects! Download it, customize it, and publish your
+                        website!
+                    </p>
                 </div>
             </div>
         </div>
         <!-- Project One Row-->
         <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
-            <div class="col-lg-6"><img class="img-fluid" src="assets/IMG-20201119-WA0075.jpg" alt="" /></div>
+            <div class="col-lg-6">
+                <img
+                    class="img-fluid"
+                    src="assets/IMG-20201119-WA0075.jpg"
+                    alt="" />
+            </div>
             <div class="col-lg-6">
                 <div class="gradient  text-center h-100 project">
                     <div class="d-flex h-100">
-                        <div class="project-text w-100 my-auto text-center text-lg-left">
-                            <h4 class="text-gradient">Misty</h4>
-                            <p class="mb-0 text-gradient">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                        <div
+                            class="project-text w-100 my-auto text-center text-lg-left">
+                            <h4 class="text-gradient">Ventosaterapia</h4>
+                            <p class="mb-0 text-gradient">
+                                Cuide bem da sua saúde, cuide bem de si. Ventosaterapia é mais que um simples tratamento... É uma experiência!
+                            </p>
                             <hr class="d-none d-lg-block mb-0 ml-0" />
+                            <a
+                                class="btn btn-primary js-scroll-trigger mt-3"                                
+                                target="_blank"
+                                href={wpLink}>Agendar uma sessão</a>
                         </div>
                     </div>
                 </div>
@@ -27,14 +53,28 @@
         </div>
         <!-- Project Two Row-->
         <div class="row justify-content-center no-gutters">
-            <div class="col-lg-6"><img class="img-fluid" src="assets/IMG_20201008_161708_817.jpg" alt="" /></div>
+            <div class="col-lg-6">
+                <img
+                    class="img-fluid"
+                    src="assets/IMG_20201008_161708_817.jpg"
+                    alt="" />
+            </div>
             <div class="col-lg-6 order-lg-first">
                 <div class="gradient text-center h-100 project">
                     <div class="d-flex h-100">
-                        <div class="project-text w-100 my-auto text-center text-lg-right">
-                            <h4 class="text-gradient">Mountains</h4>
-                            <p class="mb-0 text-gradient">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                        <div
+                            class="project-text w-100 my-auto text-center text-lg-right">
+                            <h4 class="text-gradient">Massagem</h4>
+                            <p class="mb-0 text-gradient">
+                                Nosso menu de massagens é extremamente acessível,
+                                valores mais baixos você não vai encontrar em
+                                lugar algum!
+                            </p>
                             <hr class="d-none d-lg-block mb-0 mr-0" />
+                            <a
+                                class="btn btn-primary js-scroll-trigger mt-3"                                
+                                target="_blank"
+                                href={wpLink}>Agendar uma sessão</a>
                         </div>
                     </div>
                 </div>
